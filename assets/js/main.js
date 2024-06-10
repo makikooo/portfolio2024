9c8995c0a2f152087495020797c79366073f769b
MAKIKO KARASAWA <truehope320@gmail.com>
2024-06-10 13:19:14 +0900

    CSS調整

--- assets/js/main.js
+++ assets/js/main.js
@@ -411,36 +411,3 @@ RotateCircularTextAnimation(option6);
 RotateCircularTextAnimation(option7);
 RotateCircularTextAnimation(option8);
 
-
-
-
-// GSAPで一部横スクロール
-
-gsap.registerPlugin(ScrollTrigger);
-const wrapper = document.getElementById('wrapper');
-const container = document.getElementById('container');
-
-gsap.to(container, {
-  x: () => -(container.clientWidth - wrapper.clientWidth) + 'px',
-  ease: Power0.easeNone,
-  scrollTrigger: {
-    trigger: wrapper,
-    start: 'top top',
-    end: () => {
-      if (container.clientWidth > wrapper.clientWidth) {
-        return '+=' + (container.clientWidth - wrapper.clientWidth);
-      } else {
-        return '+=' + 0;
-      }
-    },
-    pin: true,
-    anticipatePin: 1,
-    scrub: 0,
-    invalidateOnRefresh: true
-  }
-});
-
-window.addEventListener('resize', () => {
-  ScrollTrigger.refresh();
-});
-
